13d91f968a1f315c86583fcb8b12febd73c4b821
wang <wang@163.com>
2021-02-06 22:45:42 +0800

    项目初始化

---
+++ src/main/java/com/learn/jenkins_docker/JenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.learn.jenkins_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/jenkins_docker/JenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.jenkins_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
